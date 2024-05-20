e864b9f15677cecfc7efad779bafbddf7ee31d98
4aromut <a82641223@gmail.com>
2024-05-20 20:10:17 +0300

    Teacher version

--- main.py
+++ main.py
@@ -1,44 +1,33 @@
-class Task:
-    def __init__(self, description, deadline):
-        self.description = description
-        self.deadline = deadline
-        self.status = "Not completed"
-
-class TaskManager:
+class Task():
     def __init__(self):
         self.tasks = []
 
-    def add_task(self, task):
-        self.tasks.append(task)
+    def add_task(self, deadline, description):
+        self.tasks.append({'deadline': deadline, 'description': description,
+                           'status': "не выполнено"})
 
-    def mark_task_completed(self, task_description):
+    def complete_tasks(self, description):
         for task in self.tasks:
-            if task.description == task_description:
-                task.status = "Completed"
-                break
+            if task['description'] == description:
+                task['status'] = "Выполнено"
+                print(f"Задача {description} выполнена")
+            else:
+                print(f"Задача {description} не найдена")
 
-    def show_current_tasks(self):
-        current_tasks = [task for task in self.tasks if task.status == "Not completed"]
-        if len(current_tasks) == 0:
-            print("No current tasks")
-        else:
-            for task in current_tasks:
-                print(f"Description: {task.description}, Deadline: {task.deadline}, Status: {task.status}")
+    def show_tasks(self):
+        print("текущие задачи")
+        for task in self.tasks:
+            if task['status'] == "не выполнено":
+                print(f"{task['description']} - {task['deadline']}")
 
-# Пример использования
-task_manager = TaskManager()
+t = Task()
+t.add_task("01.06.2023", "Прочитать книгу")
+t.add_task("05.06.2023", "Пробежать марафон")
+t.add_task("27.06.2023", "Починить машину")
+
+t.show_tasks()
+
+t.complete_tasks("Прочитать книгу")
 
 
-task1 = Task("Do laundry", "2022-01-20")
-task2 = Task("Buy groceries", "2022-01-25")
-task3 = Task("Finish project", "2022-02-10")
 
-task_manager.add_task(task1)
-task_manager.add_task(task2)
-task_manager.add_task(task3)
-
-task_manager.show_current_tasks()
-
-task_manager.mark_task_completed("Do laundry")
-
-task_manager.show_current_tasks()
